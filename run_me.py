42ccb8a9db3630111dc12e63453db37cd86d0dba
Keegz-dz <keegz29.dz@gmail.com>
2025-04-12 16:11:00 +0530

    Add comment to clarify text argument requirement for audio cloning

--- run_me.py
+++ run_me.py
@@ -6,6 +6,7 @@ audio_path = r"C:\Users\anike\Downloads\spanish_output.wav"
 wav, sample_rate = librosa.load(audio_path, sr = 16000)
 
 main = Main()
+# !Always give a text argument or else it will default to Whisper which might break the code on some windows devices.
 wav = main.clone_audio(wav, use_vocoder=True, text = "The sky is blue and birds are flying.")
 
 sf.write("output1.wav", wav, sample_rate)
